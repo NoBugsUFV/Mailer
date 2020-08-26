7b404526625cfcfd7735616c7c6ddf364dc7f5d6
Thiago <thiagofp707@gmail.com>
2020-08-26 19:17:39 -0300

    Classe Page pronta

--- vendor/api/Page.php
+++ vendor/api/Page.php
@@ -13,13 +13,55 @@
             'data' => []
         ];
 
-        public function __construct($options = [], $tplDir="/views/"){
-            this->options = array_merge(this->defaults, $options);
+        //A função abaixo cria uma ligação entre os valores passados no array com os dados da página e 
+        //os valores chamados no template pelo rain.
+
+        private function setData($data = []){
+            foreach ($data as $key => $value) {
+                $this->tpl->assign($key, $value);
+            }
+        }
+
+        //A função abaixo configura o rain Tpl
+
+        private function tplConfig($options, $tplDir){
+            $this->options = array_merge($this->defaults, $options);
 
             $config = [
-                "tplDir" => $_SERVER["DOCUMENT_ROOT"].$tplDir
-                // Continua...
+                "tplDir" => $_SERVER["DOCUMENT_ROOT"].$tplDir,
+                "cacheDir" => $_SERVER["DOCUMENT_ROOT"]."/views-cache/"
             ];
+
+            Tpl::configure($config);
+        }
+
+        //O construtor chama a função que configura o rain e renderiza o cabeçalho da página.
+
+        public function __construct($options = [], $tplDir="/views/"){
+            
+            tplConfig($options, $tplDir);
+
+            $this->tpl = new Tpl;
+
+            $this->setData($this->options["data"]);
+            if ($this->options["header"] === true) {
+                $this->tpl->draw("header");
+            }
+        }
+
+        // A função setTpl é responsável por renderezizar o conteúdo da página (entre cabeçalho e rodapé).
+
+        public function setTpl($name, $data = [], $returnHTML = false){
+            $this->setData($data);
+            return $this->tpl->draw($name, $returnHTML);
+        }
+
+        //O destrutor é responsável por renderizar o rodapé da página.
+
+        public function __destruct(){
+            if ($this->options["footer"]===true) {
+                $this->tpl->draw("footer");  
+            }
         }
 
     }
