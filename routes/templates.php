6d71a43742ee24505c1481ebcdec5bf1e6b4c37d
Thiago <thiagofp707@gmail.com>
2020-09-02 17:47:16 -0300

    Classe Template pronta sem testar

--- routes/templates.php
+++ routes/templates.php
@@ -2,6 +2,7 @@
 
     use \Api\Page;
     use Api\Controller\User;
+    use Api\Controller\Template;
 
     $app->get('/', function () {
         User::verifyLogin();
@@ -19,4 +20,44 @@
         User::verifyLogin();
         $page = new Page();
         $page->setTpl('uploadFile');
+    });
+    $app->post('/templates/new', function(){
+        User::verifyLogin();
+        $template = new Template();
+        $template->setData($_POST);
+        $template->save();
+        header("Location: /templates");
+ 	    exit;
+    });
+
+    $app->get("/templates/{idTemplate}", function($req, $res, $args){
+        User::verifyLogin();
+        $idTemplate = $args["idTemplate"];
+        $template = new Template();
+        $template->getById((int)$idTemplate);
+        $page = new Page();
+        $page->setTpl("template", [
+            'template'=>$template->getValues()
+        ]);
+    });
+
+    $app->post("/templates/{idTemplate}", function($req, $res, $args){
+        User::verifyLogin();
+        $idTemplate = $args["idTemplate"];
+        $template = new Template();
+        $template->setData($_POST);
+        $template->update((int)$idTemplate);
+        header("Location: /templates");
+ 	    exit;
+
+    });
+
+    $app->get("/templates/{idTemplate}/delete", function($req, $res, $args){
+        $idTemplate = $args["idTemplate"];
+        User::verifyLogin();
+        $template = new Template();
+        $template->getById($idTemplate);
+        $template->delete();
+        header("Location: /templates");
+        exit;
     });
--- vendor/api/Controller/Template.php
+++ vendor/api/Controller/Template.php
@@ -6,7 +6,57 @@ use Api\DB\Sql;
 use Api\Model;
 
 class Template extends Model{
-    public function __construct(){
-        //
+    
+    public static function listAll(){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM templates ORDER BY idTemplates");
+    }
+
+    public static function checkList($templates){
+		foreach ($templates as &$template) {	
+			$t = new Template();
+			$t->setData($template);
+			$template = $t->getValues();
+		}
+		return $templates;
+    }
+    
+    public function getById($id){
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM templates WHERE idTemplate = :id", [
+            ':id'=>$id
+        ]);
+        $this->setData($results[0]);
+    }
+
+    public function save(){
+        $sql = new Sql();
+
+        $sql->query("INSERT INTO templates (nameTemplate, pathTemplate, typeTemplate) values ( :name, :path, :type)", [
+            ':name'=>$this->getnameTemplate(),
+            ':path'=> $this->getpathTemplate(),
+            ':type'=> $this->gettypeTemplate()
+        ]); 
+        $this->setData();
+    }
+
+    public function update($id){
+        $sql = new Sql();
+
+        $sql->query("UPDATE templates SET nameTemplate = :name, pathTemplate =  :path, typeTemplate = :type WHERE idRecipient = :id", [
+            ':name'=>$this->getnameTemplate(),
+            ':path'=> $this->getemailTemplate(),
+            ':type'=> $this->gettagTemplate(),
+            ':id'=> $id
+        ]);
+        $this->setData();
+    }
+
+    public function delete(){
+        $sql = new Sql();
+        $sql->query("DELETE FROM templates WHERE idTemplate = :id", [
+            ":id"=>$this->getidTemplate()
+        ]);
     }
 }
